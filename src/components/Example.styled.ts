a31cbf1719fbe8e75512a89693fb5d43a3931085
nqhung291 <nqhung291@gmail.com>
2020-11-10 21:13:04 +0700

    add example

---
+++ src/components/Example.styled.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components'
+const ExampleStyled = styled.div``
+export default ExampleStyled
---
+++ src/components/Example.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ExampleStyled from './Example.styled'
+
+const Example: React.FC = () => {
+  return <ExampleStyled>abc</ExampleStyled>
+}
+
+export default Example
