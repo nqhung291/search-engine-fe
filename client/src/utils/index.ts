d28d745b0a8e8d9c248598a68e076ebe0c1de56c
nqhung291 <nqhung291@gmail.com>
2020-12-22 21:22:10 +0700

    fix search

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,5 +1,5 @@
 import { IFormValue } from '@types'
 
 export const buildQueryParams = (formValues: IFormValue) => {
-  return `topic:"${formValues.topic}" and (title:"${formValues.search}" or content:"${formValues.search}")`
+  return `topic:"${formValues.topic}" and (title:${formValues.search} or content:${formValues.search})`
 }
