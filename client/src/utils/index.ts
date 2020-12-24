d91f2595ed62956e8fa4d56561f1c53000f30310
nqhung291 <nqhung291@gmail.com>
2020-12-24 09:32:22 +0700

    bypass cors

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,5 +1,9 @@
 import { IFormValue } from '@types'
 
 export const buildQueryParams = (formValues: IFormValue) => {
-  return `topic:"${formValues.topic}" and (title:${formValues.search} or content:${formValues.search})`
+  return `topic:"${formValues.topic}"^2 and (
+            title:${formValues.search} or 
+            title:"${formValues.search}"^1.5 or 
+            content:${formValues.search} or
+            content:"${formValues.search}"^1.5)`
 }
